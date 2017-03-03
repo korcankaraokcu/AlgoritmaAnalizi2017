0aa2ba198044627c613638a98d1ff7b4e5cb48a7
Korcan Karaokçu <korcankaraokcu@gmail.com>
2017-03-03 12:44:25 +0200

    Update pow_logn.c

--- Hafta2/pow_logn.c
+++ Hafta2/pow_logn.c
@@ -5,17 +5,18 @@
 int power(int x, unsigned int y)
 {
     int res = 1;     // Initialize result
- 
+    int counter=0;
     while (y > 0)
     {
         // If y is odd, multiply x with result
         if (y & 1)
             res = res*x;
- 
+        counter++;
         // n must be even now
         y = y>>1; // y = y/2
         x = x*x;  // Change x to x^2
     }
+    printf("Adim sayisi:%d\n",counter);
     return res;
 }
  
@@ -25,7 +26,7 @@ int main()
     int x = 3;
     unsigned int y = 2;
  
-    printf("Power is %d", power(x, y));
+    printf("Power is %d\n", power(x, y));
     system("PAUSE");	
     return 0;
 }
